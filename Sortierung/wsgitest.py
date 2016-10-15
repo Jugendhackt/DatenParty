5fc87d1a76c511ce1d2753edbf534f4e265495bf
JanMoritzMeyer <janmoritzmeyer@yahoo.com>
2016-10-15 23:54:39 +0200

    Änderungen am wsgi Script

--- Sortierung/wsgitest.py
+++ Sortierung/wsgitest.py
@@ -1,5 +1,3 @@
-from wsgiref.simple_server import make_server
-
 def application(environ, start_response):
     status = '200 OK'
     output = environ["PATH_INFO"]
@@ -9,9 +7,3 @@ def application(environ, start_response):
     start_response(status, response_headers)
 
     return [output]
-
-httpd = make_server('', 8000, application)
-print "Serving HTTP on port 8000..."
-
-# Respond to requests until process is killed
-httpd.serve_forever()
