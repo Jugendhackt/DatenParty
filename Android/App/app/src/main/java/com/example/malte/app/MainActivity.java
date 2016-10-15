7a1f7ba6a8f36b85c611d3be68ec1a5a73749548
Noah <noah.diemel@gmx.de>
2016-10-15 13:11:56 +0200

    simple PlaceHolder View

--- Android/App/app/src/main/java/com/example/malte/app/MainActivity.java
+++ Android/App/app/src/main/java/com/example/malte/app/MainActivity.java
@@ -3,16 +3,24 @@ package com.example.malte.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mindorks.placeholderview.PlaceHolderView;
+
 public class MainActivity extends AppCompatActivity {
 
-
-
-
-
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PlaceHolderView mGalleryView = (PlaceHolderView)findViewById(R.id.galleryView);
+
+// (Optional): If customisation is Required then use Builder with the PlaceHolderView
+// placeHolderView.getBuilder()
+//      .setHasFixedSize(false)
+//      .setItemViewCacheSize(10)
+//      .setLayoutManager(new GridLayoutManager(this, 3));
+
+        mGalleryView
+                .addView(new Item(this.getApplicationContext(), mGalleryView, "https://raw.githubusercontent.com/Jugendhackt/DatenParty/master/logo.png"));
     }
 }
