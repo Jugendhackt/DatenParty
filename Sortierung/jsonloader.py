77c5f5650ba89c6349e169f975a29970690356a0
JanMoritzMeyer <janmoritzmeyer@yahoo.com>
2016-10-15 14:25:23 +0200

    gitignore und python script

--- Sortierung/jsonloader.py
+++ Sortierung/jsonloader.py
@@ -1,4 +1,10 @@
 import requests
-r = requests.get('http://loklak.org/api/search.json?timezoneOffset=-120&q=from%3Atagesschau')
+r = requests.get('http://loklak.org/api/search.json?timezoneOffset=-120&q=from%3Acnnbrk')
 
-print r.json()
+loklakjson = r.json()
+statusescounter = loklakjson["search_metadata"]["count"]
+statusescounter = int(statusescounter)
+for statuses in range(0,statusescounter):
+    loklaktext = loklakjson["statuses"][statuses]["text"]
+    if not loklaktext.startswith("@"):
+        print loklaktext
