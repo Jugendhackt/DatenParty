256f44c29a1035513f3c11d83598032a5e364274
JanMoritzMeyer <janmoritzmeyer@yahoo.com>
2016-10-16 00:51:05 +0200

    tweets von der Zeit

--- Sortierung/jsonloader.py
+++ Sortierung/jsonloader.py
@@ -3,6 +3,7 @@ import json
 import datetime
 import random
 r = requests.get('http://loklak.org/api/search.json?timezoneOffset=-120&q=from%3Atagesschau')
+t = requests.get('http://loklak.org/api/search.json?timezoneOffset=-120&q=from%3Azeitonline')
 
 def hashashtag(tweet):
     wordsoftweet = tweet.split()
@@ -16,7 +17,10 @@ def hashashtag(tweet):
 
 tweetlist = []
 loklakjson = r.json()
+zeitjson = t.json()
+statuseszeitcounter = zeitjson["search_metadata"]["count"]
 statusescounter = loklakjson["search_metadata"]["count"]
+statuseszeitcounter =int(statuseszeitcounter) 
 statusescounter = int(statusescounter)
 for statuses in range(0,statusescounter):
     loklaktext = loklakjson["statuses"][statuses]["text"]
@@ -32,6 +36,20 @@ for statuses in range(0,statusescounter):
     if not loklaktext.startswith("@") and not hashashtag(loklaktext):
         tweet = {"tweet":loklaktext, "date":createdat, "profilname":name, "profilimage":profilimageurl, "tweetid":id, "tweetlink":link, "trustlevel":trustlevel}
         tweetlist.append(tweet)
+for statusescounterzeit in range(0,statuseszeitcounter):
+    zeittext = zeitjson["statuses"][statusescounterzeit]["text"]
+    createdatzeit = zeitjson["statuses"][statusescounterzeit]["created_at"]
+    createdatzeit = createdatzeit[:-5]
+    createdatzeit = datetime.datetime.strptime(createdatzeit,"%Y-%m-%dT%H:%M:%S")
+    createdatzeit = createdatzeit.strftime("%H:%M:%S")
+    namezeit = zeitjson["statuses"][statuseszeitcounter]["user"]["screen_name"]
+    profilimageurlzeit = zeitjson["statuses"][statuseszeitcounter]["user"]["profile_image_url_https"]
+    linkzeit = zeitjson["statuses"][statuseszeitcounter]["link"]
+    trustlevelzeit = random.uniform(0.2, 1.0)
+    idzeit = zeitjson["statuses"][statuseszeitcounter]["id_str"]
+    if not loklaktext.startswith("@") and not hashashtag(zeittext):
+        zeittweets = {"tweet":zeittext, "date":createdatzeit, "profilname":namezeit, "profilimage":profilimageurlzeit, "tweetid":idzeit, "tweetlink":linkzeit, "trustlevel":trustlevelzeit}
+        tweetlist.append(zeittweets)
 fp = open("tweets.json", "w")
 json.dump(tweetlist, fp)
 fp.close()       
